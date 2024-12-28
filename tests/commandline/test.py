370e00cd90054238440240fb40d0ed826eb5ec0f
vlazzarini <victor.lazzarini@mu.ie>
2024-12-28 20:11:03 +0000

    added new test

--- tests/commandline/test.py
+++ tests/commandline/test.py
@@ -183,6 +183,7 @@ def runTest():
     ["test_schedule_named_instance.csd", "testing schedule with named instr instance"],
     ["test_instr_type_var_new_compilation.csd", "testing schedule of named instr in new compilations"],
     ["test_ambiguous_opcall.csd", "test ambiguous opcall examples"],
+    ["test_opcode_type.csd", "tests opcode type"]
     ]
 
     arrayTests = [["arrays/arrays_i_local.csd", "local i[]"],
---
+++ tests/commandline/test_opcode_type.csd
@@ -0,0 +1,24 @@
+<CsoundSynthesizer>
+<CsOptions>
+-n 
+</CsOptions>
+<CsInstruments>
+0dbfs=1
+
+instr 1
+   osc:OpcodeRef init "oscili"
+   opcodeinfo osc
+   oobj:Opcode create osc
+   opcodeinfo oobj
+
+   a1 init oobj, p4, p5, p6
+      perf oobj
+      out a1
+endin
+
+</CsInstruments>
+<CsScore>
+i1 0 1 0.5 440 -1
+</CsScore>
+</CsoundSynthesizer>
+
